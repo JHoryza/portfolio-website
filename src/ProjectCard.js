e05ea7e513bcd4fdf59d807c32bf3c14cb676138
Joe Horyza <joseph.mn27@yahoo.com>
2021-04-12 11:58:14 -0500

    Added ProjectCard component

---
+++ src/ProjectCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { render } from 'react-dom';
+
+class ProjectCard extends React.Component {
+    render() {
+        return (
+            <div className="card" style={{ backgroundImage: `url(${this.props.img})` }}>
+                <div className="card-info">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-8">
+                                <h1>{this.props.name}</h1>
+                                {this.props.tags.map((tag) => (
+                                    <span class="badge bg-secondary">{tag}</span>
+                                ))}
+                            </div>
+                            <div className="col-4">
+                                <button type="button" className="btn">GitHub</button>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="row">
+                            <p>
+                                {this.props.description}
+                            </p>
+                        </div>
+                        <div className="row">
+                            <a href="">Click here to see more...</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ProjectCard
