5df22230da49588058eb6e119cbbb33a8039ac53
Joe Horyza <joseph.mn27@yahoo.com>
2021-04-29 13:16:55 -0500

    Updated project descriptions

--- src/Projects.js
+++ src/Projects.js
@@ -12,11 +12,11 @@ function Projects() {
                 <div className="row project-wrap">
                     {/* Add project cards here */}
                     <ProjectCard name="Cultivate" tags={['Android', 'Java', 'SQL']}
-                        description="An app for helping you cultivate new habits to improve your life. Care for plants that represent the habits you'd like to maintain."
+                        description="An app for helping you cultivate new habits to improve your life. Care for plants that represent the habits you'd like to maintain. User defined habits are stored locally using SQLite."
                         img="./img/project1.jpg"
                         repo="https://github.com/JHoryza/cultivate-app" />
-                    <ProjectCard name="Joker's Wild" tags={['REACT', 'REST API', 'JavaScript']}
-                        description="A REACT based hub for traditional card games. All deck functions such as shuffling and drawing are handled using an external API. Currently supports Blackjack."
+                    <ProjectCard name="Blackjack" tags={['REACT', 'REST API', 'JavaScript']}
+                        description="A Blackjack browser game built using REACT component architecture. Deck shuffle and card draw are implemented using an external card deck API."
                         img="./img/project2.jpg"
                         repo="https://github.com/JHoryza/jokers-wild" />
                     <ProjectCard name="Cultivate" tags={['Android', 'Java', 'SQL']}
