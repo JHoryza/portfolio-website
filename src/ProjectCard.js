266dd1720fb934252ac0d06fa3b7508e3f303a52
Joe Horyza <joseph.mn27@yahoo.com>
2021-05-24 12:11:07 -0500

    Reformatted project cards to accomodate lengthy titles

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -8,14 +8,11 @@ class ProjectCard extends React.Component {
                 <div className="card-info">
                     <div className="container">
                         <div className="row">
-                            <div className="col-8">
-                                <h1>{this.props.name}</h1>
+                            <div className="col-12">
+                                <h3>{this.props.name}</h3>
                                 {this.props.tags.map((tag) => (
                                     <span class="badge bg-secondary">{tag}</span>
                                 ))}
-                            </div>
-                            <div className="col-4">
-                                <a class="btn" href={this.props.repo} target="_blank" role="button">GitHub</a>
                             </div>
                         </div>
                         <hr />
@@ -26,6 +23,9 @@ class ProjectCard extends React.Component {
                         </div>
                         <div className="row">
                             <a href="">Click here to see more...</a>
+                            <div className="col-4">
+                                <a class="btn" href={this.props.repo} target="_blank" role="button">GitHub</a>
+                            </div>
                         </div>
                     </div>
                 </div>
