7805ac0f2df90888234946715b65cde5b003dcc6
Joe Horyza <joseph.mn27@yahoo.com>
2021-05-28 11:08:49 -0500

    Updated about me content

--- src/About.js
+++ src/About.js
@@ -10,10 +10,8 @@ function About() {
                     </div>
                     <div className="col-md-5">
                         <h1>About me</h1>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In eveniet commodi reprehenderit,
-                        unde
-                        laborum dignissimos voluptatibus cumque neque dolorem assumenda dicta impedit doloremque
-                        repellat minus quia debitis quasi consequatur pariatur.</p>
+                        <h6><i>Creative minded. Self-driven. Team player.</i></h6>
+                        <p>I am a software developer motivated by a chance to create, solve challenging problems, and learn new technologies. I developed a passion for programming from a young age and began to turn that passion into a career in 2016. My formal education is in Computer Science and I have a professional background in Industrial Engineering. Solving problems and implementing logic driven solutions has been my work for the last three years. Now I am channeling that experience as I make the transition into the software industry. </p>
                         <ul className="list-group list-group-horizontal">
                             <li className="list-group-item">
                                 <i className="bi bi-stack"> Java</i>
@@ -22,7 +20,7 @@ function About() {
                                 <i className="bi bi-stack"> Android</i>
                             </li>
                             <li className="list-group-item">
-                                <i className="bi bi-stack"> Web Front-end</i>
+                                <i className="bi bi-stack"> HTML/CSS/JS</i>
                             </li>
                             <li className="list-group-item">
                                 <i className="bi bi-stack"> SQL</i>
